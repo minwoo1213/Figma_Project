5a0402efddb82c5fb65e7a67a26a16f4f7c5d5fd
minwoo._.exfp <103166594+minwoo1213@users.noreply.github.com>
2025-02-08 16:50:49 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Cloudflare Pages에서 경로 문제 해결
+  build: {
+    outDir: 'dist'
+  }
 });
